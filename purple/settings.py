f192d905092a9ac760b566b7e0864d503ede534d
KamilGaribov <kamilgarib@gmail.com>
2020-11-17 15:56:46 +0400

    staticdir

--- purple/settings.py
+++ purple/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/api.purplecakeboutique.az/static/"
+STATIC_ROOT = "/var/www/api.purplecakeboutique.az/static/"
 
 MEDIA_URL = env("MEDIA_URL")
 MEDIA_ROOT = env("MEDIA_ROOT")
