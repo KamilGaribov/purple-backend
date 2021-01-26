65fc4b9b6da021ceb6cb0735e62c5d4c227c05bc
KamilGaribov <kamilgarib@gmail.com>
2021-01-26 16:55:13 +0400

    cors

--- purple/settings.py
+++ purple/settings.py
@@ -53,6 +53,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://159.89.19.237:3000',
     'http://127.0.0.1:3000',
     'http://0.0.0.0:3000',
+    'https://purplecakeboutique.az'
 ]
 
 # Application definition
