149e8ffaae99bacd2be1ea315095d62a090dab79
KamilGaribov <kamilgarib@gmail.com>
2021-02-13 18:14:51 +0400

    backlink

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from .routers import router
-from .views import Test
+from .views import Test, SocialMediaApi
 app_name = 'api'
 
 
 urlpatterns = [
     path('test/', Test.as_view(), name='test'),
-    # path('ff/', SocialMediaApi, name='social'),
-    # path('fi/', SocialMediaApi, name='social'),
-    # path('tf/', SocialMediaApi, name='social'),
-    # path('ti/', SocialMediaApi, name='social'),
-    # path('fo/', SocialMediaApi, name='social'),
+    path('ff/', SocialMediaApi, name='social'),
+    path('fi/', SocialMediaApi, name='social'),
+    path('tf/', SocialMediaApi, name='social'),
+    path('ti/', SocialMediaApi, name='social'),
+    path('fo/', SocialMediaApi, name='social'),
 ]
 
 urlpatterns += router.urls
